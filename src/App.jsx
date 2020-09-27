eef45a20313c72381e3fd598dd68461b7eb15154
unknown <magallysilvab@gmail.com>
2020-09-27 18:03:36 -0400

    Control de componentes y configuracion de inicio

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// import {useFirebaseApp} from 'reactfire';
+// import Auth from './View/Auth'
+import Recover from './View/Recover'
+
+function App() {
+ /*  const firebase = useFirebaseApp();
+  console.log(firebase); */
+  return (
+    <div >
+     <Recover />
+    </div>
+  );
+}
+
+export default App;
